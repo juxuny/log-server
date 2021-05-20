e21f668f84aab465a3ac414885105f920634b59c
juxuny <juxuny@gmail.com>
2021-05-20 23:05:43 +0800

    docker-compose.yaml

---
+++ cleaner-entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/app/cleaner --dir log --keep 1d --ext log --script-file compress.sh
---
+++ compress.sh
@@ -0,0 +1,3 @@
+tar czf "${FILE_NAME}".tar.gz "${FILE_NAME}" && \
+echo "rm ${FILE_NAME}" && \
+rm "${FILE_NAME}"
